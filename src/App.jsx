0b3f078e032789534f40381612226342ba9f0412
DevDan <ogunniyioreyemi@gmail.com>
2024-06-12 05:05:50 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
+import Cert from "./components/Cert";
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Cert />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="h-screen bg-black flex justify-center items-center">
+      <div className="w-[60%]">
+        <div className="w-full h-[30rem] bg-heroimage"></div>
+        <h1 className="font-black text-5xl p-2 text-center">Oniosun Obafemi</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
